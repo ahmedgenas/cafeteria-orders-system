2a636d287f4ed92cc6d3a1cf97235f4ef953c0ee
ahmedgenas <78415203+ahmedgenas@users.noreply.github.com>
2025-08-19 17:02:10 +0200

    Add migrations/6/down.sql 1755619415754

---
+++ migrations/6/down.sql
@@ -0,0 +1,3 @@
+
+DELETE FROM products WHERE name IN ('Coffee', 'Tea', 'Juice', 'Dessert', 'Turkish Coffee', 'Espresso', 'Cappuccino', 'American Coffee', 'Black Tea', 'Green Tea', 'Mint Tea', 'Herbal Tea');
+DELETE FROM cups WHERE name IN ('Ceramic Cup', 'Plastic Cup', 'Large Glass');
